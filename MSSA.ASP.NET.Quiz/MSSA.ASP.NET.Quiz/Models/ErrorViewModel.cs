270a53b567880256b8bd7077624dc22e26880825
Julian Edington <arcanicvoid@gmail.com>
2021-03-13 00:53:32 -0600

    Add files via upload

---
+++ MSSA.ASP.NET.Quiz/MSSA.ASP.NET.Quiz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSSA.ASP.NET.Quiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
