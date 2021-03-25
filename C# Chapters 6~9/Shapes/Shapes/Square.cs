81012425a3dc5b3b1055861faa70346f35d5ba74
Julian Edington <arcanicvoid@gmail.com>
2021-03-24 22:42:49 -0500

    Add files via upload

---
+++ C# Chapters 6~9/Shapes/Shapes/Square.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Square : Shape
+    {
+        // fields
+        protected double Side = 1.0;
+
+
+        // constructors
+        public Square(double side, string color)
+            : base(color) // DRY
+            => SetSide(side); 
+        // Square const ends 
+
+
+        // methods
+        public double GetArea()
+            => Side * Side;
+        // GetArea method ends
+
+        public double GetPerimeter()
+            => 4 * Side;
+        // GetPerimeter method ends
+        
+        public double GetSide()
+            => Side;
+        //  GetSide method ends
+
+        public void SetSide(double side)
+        {
+            if (side < 0)
+            {
+                side = Math.Abs(side);
+            }
+            else if (side == 0)
+            {
+                side = 1.0;
+            }
+            Side = side;
+        } //  SetSide method ends
+
+        public override string ToString()
+            => $"Square side value = {Side}, {base.ToString()}";
+        // ToString method ends
+    } // class ends
+} // namespace ends
---
+++ C# Chapters 6~9/Shapes/Shapes/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Triangle : Shape
+    {
+        // fields
+        protected double SideA = 1.0;
+        protected double SideB = 1.0;
+        protected double SideC = 1.0;
+
+
+        // constructors
+        public Triangle(double a, double b, double c, string color)
+            : base(color) // DRY
+            => SetSides(SideA, SideB, SideC);
+        // Triangle const ends
+
+
+        // methods
+        public double GetArea()
+            //disabled//  => Math.Sqrt (GetPerimeter() / 2 * (GetPerimeter() / 2 - sideA) * (GetPerimeter() / 2 - sideB) * (GetPerimeter() / 2 - sideC));
+        {
+            double hPer = GetPerimeter() / 2;
+            return Math.Sqrt(hPer * (hPer - SideA) * (hPer - SideB) * (hPer - SideC));
+        } // GetArea method ends
+
+        public double GetPerimeter()
+            => SideA + SideB + SideC;
+        // GetPerimeter method ends
+
+        public (double sideA, double sideB, double sideC) GetSides()
+            => (SideA, SideB, SideC);
+        // GetSides method ends
+
+        public void SetSides(double a, double b, double c)
+        {
+            if ((a + b) > c && (a + c) > b && (b + c) > a)
+            {
+                SideA = a;
+                SideB = b;
+                SideC = c;
+            }
+        }// SetSides method ends
+
+        public override string ToString() 
+            =>  $"Triangle side values... Side A = {SideA}, Side B = {SideB}, Side C = {SideC}, {base.ToString()}"; 
+        // ToString method ends
+    } // class ends
+} // namespace ends
