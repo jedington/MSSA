3fe76549dddfe0b1139b60283cf77d9291cccdc8
Julian Edington <arcanicvoid@gmail.com>
2021-03-24 22:59:25 -0500

    Add files via upload

---
+++ ASP.NET Part 1/HelloWorld.ASPNET/HelloWorld.ASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorld.ASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
