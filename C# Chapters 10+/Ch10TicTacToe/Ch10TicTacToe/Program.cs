ec825f75e97cf919d4b1274c8b9c55df9de0261a
Julian Edington <arcanicvoid@gmail.com>
2021-03-30 21:41:51 -0500

    Add files via upload

---
+++ C# Chapters 10+/Ch10TicTacToe/Ch10TicTacToe/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+
+namespace Ch10TicTacToe
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[,] ticTacToeBoard =
+            {
+                {'-','-','-'},
+                {'-','-','-'},
+                {'-','-','-'}
+            };
+            int winX = 0, winO = 0;
+
+            for (int turns = 0; turns < 4; turns++)
+            {
+                UserInput(ticTacToeBoard, 'X');
+                UserInput(ticTacToeBoard, 'O');
+                (winX, winO) = CheckWin(ticTacToeBoard);
+                if (winX == 1 && winO == 0)
+                {
+                    Console.Write($"\nPlayer X wins!");
+                    return;
+                }
+                if (winX == 0 && winO == 1)
+                {
+                    Console.Write($"\nPlayer O wins!");
+                    return;
+                }
+            }
+            if (winX == 0 && winO == 0)
+            {
+                Console.Write($"\nIt's a Draw!");
+            }
+        } // Main method ends
+
+        static void UserInput(char[,] ticTacToeBoard, char player)
+        {
+            bool check = false;
+            while (check == false)
+            {
+                try
+                {
+                    start_of_loop:
+                    Console.Write($"\nPlayer {player}, Choose a row: ");
+                    int row = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"\nPlayer {player}, Choose a column: ");
+                    int col = Convert.ToInt32(Console.ReadLine());
+                    if (ticTacToeBoard[row, col] == 'X' || ticTacToeBoard[row, col] == 'O')
+                    {
+                        Console.Write($"\nInvalid... spot is taken. Try Again, Player {player}");
+                        goto start_of_loop;
+                    }
+                    if (player == 'X')
+                    {
+                        ticTacToeBoard[row, col] = 'X';
+                    }
+                    else if (player == 'O')
+                    {
+                        ticTacToeBoard[row, col] = 'O';
+                    }
+                    UseBoard(ticTacToeBoard);
+                    check = true;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine($"{fe.Message}... Try again, Player {player}.");
+                }
+                catch (SystemException se)
+                {
+                    Console.WriteLine($"{se.Message}... Try again, Player {player}.");
+                }
+            }
+        } // UserInput method ends
+
+        static void UseBoard(char[,] ticTacToeBoard)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(ticTacToeBoard[row, col]);
+                }
+                Console.WriteLine();
+            }
+        } // UseBoard method ends
+
+        static (int winX, int winO) CheckWin(char[,] ticTacToeBoard)
+        {
+            // Wanted to find an alternative, rather than hard-typing each if-statement (DRY).
+            // Considering I used an outside source, I'm cool with not getting a bonus here.
+            // Source/help: https://stackoverflow.com/questions/47115924/displaying-xs-and-os-for-c-sharp-tic-tac-toe
+            ILookup<string, string> winnerWinnerChickenDinner 
+                = Enumerable.Range(0, 3)
+                .SelectMany(n => new[] // check rows & cols //
+                {
+                    Enumerable.Range(0, 3)
+                        .Select(x => ticTacToeBoard[n, x]),
+                    Enumerable.Range(0, 3)
+                        .Select(x => ticTacToeBoard[x, n]),
+                })
+                .Concat(new[] // check diagonals //
+                {
+                    Enumerable.Range(0, 3)
+                        .Select(x => ticTacToeBoard[x, x]),
+                    Enumerable.Range(0, 3)
+                        .Select(x => ticTacToeBoard[x, 2 - x]),
+                })
+                // chars => string //
+                .Select(joinChars => String.Join($"", joinChars)) 
+                // returns Lookup //
+                .ToLookup(x => x); 
+
+            return (winnerWinnerChickenDinner["XXX"].Count(), winnerWinnerChickenDinner["OOO"].Count());
+        } // CheckWin method ends
+
+        static bool DidPlayerWin(char[,] ticTacToeBoard) // not used //
+        {
+            if (ticTacToeBoard[0, 0] == 'X' && ticTacToeBoard[0, 1] == 'X' && ticTacToeBoard[0, 2] == 'X')
+            {
+                return true;
+            }
+            if (ticTacToeBoard[0, 0] == 'O' && ticTacToeBoard[0, 1] == 'O' && ticTacToeBoard[0, 2] == 'O')
+            {
+                return true;
+            }
+            return false;
+        } // DidPlayerWin method ends
+    } // class ends
+} // namespace ends
