1c20922ac45adeb3e31a90c2ff60cec680f8dd17
Julian Edington <arcanicvoid@gmail.com>
2021-04-02 16:47:49 -0500

    Add files via upload

---
+++ C# Chapters 11+/Ch13Shapes/Shapes/Square.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Shapes
+{
+    class Square : Shape
+    {
+        // fields
+        protected double Side = 1.0;
+
+
+        // constructors
+        public Square(double side, [Optional] string color)
+            : base(color) // DRY
+            => SetSide(side);
+        // Square const ends 
+
+
+        // methods
+        public override double GetArea()
+            => Side * Side;
+        // GetArea method ends
+
+        public override double GetPerimeter()
+            => 4 * Side;
+        // GetPerimeter method ends
+        
+        public double GetSide()
+            => Side;
+        // GetSide method ends
+
+        public void SetSide(double side)
+        {
+            if (side < 0)
+            {
+                side = Math.Abs(side);
+            }
+            else if (side == 0)
+            {
+                side = 1.0;
+            }
+            Side = side;
+        } //  SetSide method ends
+
+        public override string ToString()
+            => $"Square side value = {Side}, {base.ToString()}";
+        // ToString method ends
+    } // class ends
+} // namespace ends
