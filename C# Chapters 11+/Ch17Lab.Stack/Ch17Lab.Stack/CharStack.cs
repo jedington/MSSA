b3b63653b20cbf626909e51e2741164c23d75952
Julian Edington <arcanicvoid@gmail.com>
2021-04-02 16:52:43 -0500

    Add files via upload

---
+++ C# Chapters 11+/Ch17Lab.Stack/Ch17Lab.Stack/CharStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ch17Lab.Stack
+{
+    public class CharStack
+    {
+        // fields
+        private readonly int _size; // initially 0
+        private readonly char[] _stack; // initially null
+        private int _topOfStack; // initially 0
+
+
+        // constructor
+        public CharStack(int size)
+        {
+            _size = size;
+            _stack = new char[size];
+        } // IntStack const ends
+
+
+        // methods
+        public void Push(char valueToPush)
+        {
+            if (_topOfStack < _size)
+            {
+                _stack[_topOfStack] = valueToPush;
+                _topOfStack++;
+            }
+        } // Push method ends
+
+        public char Pop()
+            => _stack[--_topOfStack];
+        // Pop method ends
+
+        public bool IsEmpty()
+            => _topOfStack <= 0;
+        // IsEmpty method ends
+
+        public bool IsFull()
+            => _topOfStack >= _size;
+        // IsFull method ends
+    } // class ends
+} // namespace ends
---
+++ C# Chapters 11+/Ch17Lab.Stack/Ch17Lab.Stack/IntStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ch17Lab.Stack
+{
+    public class IntStack
+    {
+        // fields
+        private readonly int _size; // initially 0
+        private readonly int[] _stack; // initially null
+        private int _topOfStack; // initially 0
+
+
+        // constructor
+        public IntStack(int size)
+        {
+            _size = size;
+            _stack = new int[size];
+        } // IntStack const ends
+
+
+        // methods
+        public void Push(int valueToPush)
+        {
+            if (_topOfStack < _size)
+            {
+                _stack[_topOfStack] = valueToPush;
+                _topOfStack++;
+            }
+        } // Push method ends
+
+        public int Pop()
+        {
+            _topOfStack--;
+            return _stack[_topOfStack];
+        } // Pop method ends
+
+        public bool IsEmpty()
+        {
+            if (_topOfStack > 0)
+            {
+                return false;
+            }
+            return true;
+        } // IsEmpty method ends
+
+        public bool IsFull()
+        {
+            if (_topOfStack >= _size)
+            {
+                return true;
+            }
+            return false;
+        } // IsFull method ends
+    } // class ends
+} // namespace ends
