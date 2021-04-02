febd6b69536c238e425e3308fe0dc6d8e883ceb0
Julian Edington <arcanicvoid@gmail.com>
2021-04-02 16:51:12 -0500

    Add files via upload

---
+++ C# Chapter 10/Ch10Arrays.QuizPractice/QuizTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace QuizTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            Console.WriteLine("What is your favorite Letter?");
+            
+            string favLetter = Console.ReadLine();
+            char favChar = Convert.ToChar(favLetter);
+
+            int numberOfLetter = Array.IndexOf(alphabet, favChar);
+            alphabet[numberOfLetter] = Convert.ToChar(favLetter.ToUpper());
+
+            foreach (char letter in alphabet)
+            {
+                Console.Write($"{letter}, ");
+            }
+
+            Console.WriteLine("\n\nHow many favorite numbers do you have?\n");
+
+            int favNumberCount = Convert.ToInt32(Console.ReadLine());
+            int[] favNumbers = new int[favNumberCount];
+
+            for (int i = 0; i < favNumberCount; i++)
+            {
+                Console.Write("Favorite Number: ");
+                favNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int number in favNumbers)
+            {
+                Console.Write($"{number}, ");
+            }
+        }
+    }
+}
