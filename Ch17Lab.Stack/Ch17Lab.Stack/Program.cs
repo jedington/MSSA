52e938022e00901feefd2a2db64aae284298d3cd
Julian Edington <arcanicvoid@gmail.com>
2021-04-02 16:46:31 -0500

    Add files via upload

---
+++ Ch17Lab.Stack/Ch17Lab.Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ch17Lab.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntStack tenInts = new IntStack(10);
+            bool empty = tenInts.IsEmpty();
+
+            tenInts.Push(1);
+            tenInts.Push(2);
+            tenInts.Push(3);
+            tenInts.Push(4);
+            tenInts.Push(5);
+            tenInts.Push(6);
+            tenInts.Push(7);
+            tenInts.Push(8);
+            tenInts.Push(9);
+            tenInts.Push(10);
+            tenInts.Push(11);
+
+            bool full = tenInts.IsFull();
+            int v1 = tenInts.Pop();
+            int v2 = tenInts.Pop();
+        }
+    }
+}
---
+++ Ch17Lab.Stack/Ch17Lab.Stack/StringStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ch17Lab.Stack
+{
+    public class StringStack
+    {
+        // fields
+        private readonly int _size; // initially 0
+        private readonly string[] _stack; // initially null
+        private int _topOfStack; // initially 0
+
+
+        // constructor
+        public StringStack(int size)
+        {
+            _size = size;
+            _stack = new string[size];
+        } // IntStack const ends
+
+
+        // methods
+        public void Push(string valueToPush)
+        {
+            if (_topOfStack < _size)
+            {
+                _stack[_topOfStack] = valueToPush;
+                _topOfStack++;
+            }
+        } // Push method ends
+
+        public string Pop()
+            => _stack[--_topOfStack];
+        // Pop method ends
+
+        public bool IsEmpty()
+            => _topOfStack <= 0;
+        // IsEmpty method ends
+
+        public bool IsFull()
+            => _topOfStack >= _size;
+        // IsFull method ends
+    } // class ends
+} // namespace ends
