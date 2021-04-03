3a984506b8a94e07afc034b79078a06bbeb1ac64
Julian Edington <arcanicvoid@gmail.com>
2021-04-03 11:19:10 -0500

    Add files via upload

---
+++ C# Chapter 10/Ch10Arrays.Quiz/Ch10Arrays.Quiz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ch10Arrays.Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////// Ten Numbers
+            int[] tenNumbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}. Enter a Number: ");
+                tenNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("");
+            foreach (int number in tenNumbers)
+            {
+                Console.Write($"{number}, ");
+            }
+
+            /////////////////////////////////////////// Chosen Numbers
+            Console.Write("\n\nHow many numbers would you like to enter? ");
+            int numberCount = Convert.ToInt32(Console.ReadLine());
+            int[] chosenNumbers = new int[numberCount];
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                Console.Write($"{i + 1}. Enter a Number: ");
+                chosenNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int number in chosenNumbers)
+            {
+                Console.Write($"{number}, ");
+            }
+        } // main method ends
+    } // class ends
+} // namespace ends
